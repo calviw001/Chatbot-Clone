9a172b2757dfb76cb357ee7c6a9598ce81cdb5e9
calviw001 <cowvynyia@gmail.com>
2026-07-28 16:46:38 -0700

    Created a folder called 'db' and added an 'index.js' file into it
    I made this 'db/index.js' file to connect the the sql database instead of connecting the database in the 'index.js' file. I thought this made for a better structure. Also, I switched to using the 'createPool()' function for connecting to the sql database, so that I can use await and async.

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,10 @@
 import express from "express"
-import mysql from "mysql2"
 import cors from "cors"
 import dotenv from "dotenv"
 
 dotenv.config()
 
 const app = express()
-
-const db = mysql.createConnection({
-    host:"localhost",
-    user:process.env.DB_USERNAME,
-    password:process.env.DB_PASSWORD,
-    database:"chatbottest"
-})
 
 app.use(express.json({limit: "16kb"})); // Support the json in my application so that anyone can send me json data
 
