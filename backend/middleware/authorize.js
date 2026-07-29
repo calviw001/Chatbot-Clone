5f35d33e0bff56e11d93cbf4c23500fa8dc0fe47
calviw001 <cowvynyia@gmail.com>
2026-07-28 19:41:23 -0700

    Created 'middleware/authorize.js'
    This file is used to check whether there is a user currently in session or not. This will later be used to make sure no one is trying to do something unauthorized.

---
+++ backend/middleware/authorize.js
@@ -0,0 +1,11 @@
+
+export function authorizeMiddleware(req, res, next) {
+    // Let the user through if they are in session
+    if(req.session.userId && req.session.userName) {
+        return next()
+    }
+    // Return an error for unathorized access
+    else {
+        return res.status(401).json({message: "Unauthorized access."})
+    }
+}
