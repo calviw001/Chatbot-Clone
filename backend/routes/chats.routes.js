a09ad8664ac433741b7ee92866ca58c22f659131
calviw001 <cowvynyia@gmail.com>
2026-07-29 17:41:21 -0700

    Changed the status code t0 201 for when a chat is created

--- backend/routes/chats.routes.js
+++ backend/routes/chats.routes.js
@@ -37,7 +37,7 @@ router.post("/add", authorizeMiddleware, async (req, res) => {
 
         // Then add a new chat
         await db.query("INSERT INTO chats (`user_id`, `title`) VALUES (?, ?)", [userId, title])
-        return res.status(200).json({message: "A new chat was created!"})
+        return res.status(201).json({message: "A new chat was created!"})
     }
 
     catch(error) {
