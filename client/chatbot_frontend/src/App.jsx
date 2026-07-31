f9125737cd52da87a7a6bc3abffc263c30c75b3f
calviw001 <cowvynyia@gmail.com>
2026-07-31 14:47:18 -0700

    Created a new React app and installed tailwindcss
    This new React app will be the frontend for my chatbot clone. I also plan to use tailwindcss, so I made sure to install and test it.

---
+++ client/chatbot_frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='flex justify-center text-3xl bg-red-900'>Test</p>
+    </>
+  )
+}
+
+export default App
