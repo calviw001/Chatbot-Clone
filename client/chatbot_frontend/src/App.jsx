b4af246fa1dbfbcfd67aeabcd1e730511fc2db52
calviw001 <cowvynyia@gmail.com>
2026-08-02 16:04:55 -0700

    Create routes using 'react-router-dom' in 'App.jsx'
    I forgot to commit this eariler so I'm doing it now. This file returns a router to the various pages that I plan to create for this chatbot. This includes the new home page in 'home.jsx' and a chat page in 'chat.jsx'. Furthermore, the <UserContextProvider> tag is wrapped around the router, so that all of these pages have access to it variables I mentioned before. The idea is that the home page will check the 'user' variable to check whether a user is logged in or not. If not, then the home page will redirect back to the login page. The same will apply to the chat page and any other page I make that needs a logged in user to work.

--- client/chatbot_frontend/src/App.jsx
+++ client/chatbot_frontend/src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Signup from './pages/signup.jsx'
+import Login from './pages/login.jsx'
+import Home from './pages/home.jsx'
+import Chat from './pages/chat.jsx'
+import UserContextProvider from './context/UserContextProvider.jsx'
+
+
+const router = createBrowserRouter(  
+  createRoutesFromElements(
+    <Route path='/'>
+      <Route path='' element={<Home />} />          
+      <Route path='auth/signup' element={<Signup />} />
+      <Route path='auth/login' element={<Login />} />
+      <Route path='chat/:chatId' element={<Chat />} />
+      <Route path='*' element={<div>Not Found</div>} />
+    </Route>
+  )
+)
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <p className='flex justify-center text-3xl bg-red-900'>Test</p>
-    </>
+    <UserContextProvider>
+      <RouterProvider router={router} />
+    </UserContextProvider>
   )
 }
 
