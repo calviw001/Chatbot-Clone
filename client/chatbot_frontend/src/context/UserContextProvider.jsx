973a9bceb243fca0270790c35005fec75712775e
calviw001 <cowvynyia@gmail.com>
2026-08-02 15:57:06 -0700

    Created a new 'context' folder in the frontend with the files 'UserContext.js' and 'UserContextProvider.jsx'
    The former file creates and returns a React Context. The latter file creates a <UserContext.Provider> tag that wraps around some provided children components, allowing those children access to some variables. The 'user' variable holds user data of a logged in user, and the 'isLoading' variable is either true or false. The latter file will also make a get request to the 'auth/user' route from eariler and see whether is actually any user data is set 'user' equal to. Once the get request is complete 'isLoading' is set to false to represent that the request was finished.

---
+++ client/chatbot_frontend/src/context/UserContextProvider.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import UserContext from "./UserContext";
+
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = useState(null)           // Holds data of the logged in user
+    const [isLoading, setIsLoading] = useState(true) // Used to check whether the 'auth/user' route is done running or not
+
+    useEffect(() => {
+        // Check whether there is a valid session or not by calling the 'auth/user' route
+        const fetchUserData = async () => {
+            try {
+                const response = await axios.get("http://localhost:8000/auth/user", { withCredentials: true })
+                // If there is no error, then user data was found
+                // Set 'user' to the provided user data
+                setUser(response.data)
+            }
+
+            catch(error) {
+                // If there is any error. then no user data was returned
+                // Set 'user' to null to indicate there being no one logged in 
+                setUser(null)
+            }
+
+            finally {
+                // The route is finished now, so 'isLoading' is set to false
+                setIsLoading(false)
+            }
+        }
+        fetchUserData()
+    }, [])
+
+    return(
+        // Make these three variables avaliable to any component wrapped inside of it
+        <UserContext.Provider value={{ user, setUser, isLoading }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider
