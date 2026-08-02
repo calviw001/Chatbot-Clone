b8f235cd55beac0fd0a62c28bddc0ac92c93e487
calviw001 <cowvynyia@gmail.com>
2026-08-01 19:19:58 -0700

    The 'login.jsx' page should now have functionality
    This page should now take in the user's username, email, and password. The submit button at the button will login the user and send them to the home page. The text at the button will sent the user to the signup page if they are not signed up already. For now, any errors are just outputted to the console. Later on, if I have more time I'll make it so a popup with the error message shows up on screen instead.

--- client/chatbot_frontend/src/pages/login.jsx
+++ client/chatbot_frontend/src/pages/login.jsx
@@ -1,63 +1,103 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 
 const Login = () => {
-  return (
-    <div className="relative flex justify-center items-center min-h-screen w-full bg-gray-100">
 
-        <div className="relative flex flex-col justify-center items-center rounded-md drop-shadow-lg bg-white max-w-sm sm:max-w-md">
+    // Initialize all variables
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-            <h1 className="mt-4 text-2xl">Login</h1>
+    const navigate = useNavigate()
 
-            <form className="p-6 flex flex-col justify-center">
+    // Submit button functionality
+    const handleSubmit = async e => {
+        e.preventDefault()
 
-                <div className="flex flex-col mt-2">
-                    <label htmlFor="username">Username:</label>
-                    <input
-                        type="text"
-                        name="username"
-                        id="username"
-                        placeholder="username"
-                        className="w-full sm:w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-100"
-                    />
-                </div>
+        // Set up the input to the auth/login route
+        const userInput = {
+            "username": username,
+            "email": email,
+            "password": password
+        }
 
-                <div className="flex flex-col mt-2">
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="email"
-                        className="w-full sm:w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-100"
-                    />
-                </div>
+        try{
+            
+            // Attempt to login with the entered in user information
+            await axios.post("http://localhost:8000/auth/login", userInput, { withCredentials: true })
+            navigate("/")
+        } catch(error) {
+    
+            // If an error occured, print it out  
+            console.log("Failed to login: ", error)
+        }
+    }
 
-                <div className="flex flex-col mt-2">
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        placeholder="password"
-                        className="w-full sm:w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-100"
-                    />
-                </div>
+    return (
+        <div className="relative flex justify-center items-center min-h-screen w-full bg-gray-100">
 
-                <button 
-                    type="submit"
-                    className="w-full sm:w-100 mt-2 py-3 px-3 rounded-lg text-white bg-gray-500 border border-gray-500"
-                >
-                    Submit
-                </button>
+            <div className="relative flex flex-col justify-center items-center rounded-md drop-shadow-lg bg-white max-w-sm sm:max-w-md">
 
-                <p className="flex mt-2 justify-center items-center">Don't have an account? Signup!</p>
+                <h1 className="mt-4 text-2xl">Login</h1>
 
-            </form>
+                <form className="p-6 flex flex-col justify-center" onSubmit={handleSubmit}>
+
+                    <div className="flex flex-col mt-2">
+                        <label htmlFor="username">Username:</label>
+                        <input
+                            type="text"
+                            name="username"
+                            id="username"
+                            placeholder="username"
+                            className="w-full sm:w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-100"
+                            onChange={(e) => {setUsername(e.target.value)}}
+                        />
+                    </div>
+
+                    <div className="flex flex-col mt-2">
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="email"
+                            className="w-full sm:w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-100"
+                            onChange={(e) => {setEmail(e.target.value)}}
+                        />
+                    </div>
+
+                    <div className="flex flex-col mt-2">
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="password"
+                            className="w-full sm:w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-100"
+                            onChange={(e) => {setPassword(e.target.value)}}
+                        />
+                    </div>
+
+                    <button 
+                        type="submit"
+                        className="w-full sm:w-100 mt-2 py-3 px-3 rounded-lg text-white bg-gray-500 border border-gray-500"
+                        onClick={handleSubmit}
+                    >
+                        Submit
+                    </button>
+
+                    <p className="flex mt-2 justify-center items-center">Don't have an account?  
+                        <Link to="/auth/signup">Sign Up!</Link>
+                    </p>
+
+                </form>
+
+            </div>
 
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default Login
