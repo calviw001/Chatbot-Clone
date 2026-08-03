4c211f2dcf051153677d49d6e905ed015bf025e8
calviw001 <cowvynyia@gmail.com>
2026-08-03 15:33:45 -0700

    The home page in 'home.jsx' will redirect users to login page now
    Using the 'user' and 'isLoading' variables, the home page will check if there is a logged in user. If there is not, then the home page redirects to the login page.

--- client/chatbot_frontend/src/pages/home.jsx
+++ client/chatbot_frontend/src/pages/home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import {useEffect, useState, useContext} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import UserContext from '../context/UserContext'
+import Sidebar from '../components/sidebar'
+
 
 const Home = () => {
+  // Initialize all variables
+  const {user, isLoading} = useContext(UserContext)
+
+  const navigate = useNavigate()
+
+  // Activates when 'user' or 'isLoading' changes
+  useEffect(() => {
+    // If there is no user logged in after checking, redirect to the login page
+    if(!isLoading && !user) {
+      navigate("/auth/login")
+    }
+    else {
+      console.log(user)
+    }
+  }, [user, isLoading])
+
+  // Display 'Loading...' instead of the main content while waiting to check if there is a user currently logged in 
+  if (isLoading) return <div>Loading...</div>
+  if (!user) return null
+
   return (
-    <div>home</div>
+    <div>
+      Main
+    </div>
   )
 }
+
 
 export default Home
