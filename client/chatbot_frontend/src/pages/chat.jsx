eb1d47abbbf034485c516e0f286279259ea69466
calviw001 <cowvynyia@gmail.com>
2026-08-04 14:18:38 -0700

    Started on the chat page in 'chat.jsx'
    I copied most of the code from the main page, but added a 'userChatMessages' variable. Using the 'chats/messages/get/:chatId' the page will load and put all of the chat messages in the 'userChatMessages' variable.

--- client/chatbot_frontend/src/pages/chat.jsx
+++ client/chatbot_frontend/src/pages/chat.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import {useEffect, useState, useContext} from 'react'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
+import axios from 'axios'
+import UserContext from '../context/UserContext'
+import Sidebar from '../components/sidebar'
+
 
 const Chat = () => {
+  // Initialize all variables
+  const [userChatMessages, setUserChatMessages] = useState([])
+  const [isOpen, setIsOpen] = useState(true)
+  const {user, isLoading} = useContext(UserContext)
+
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const chatId = location.pathname.split("/")[2]
+
+  // Activates when 'user' or 'isLoading' changes
+  useEffect(() => {
+    // If there is no user logged in after checking, redirect to the login page
+    if(!isLoading && !user) {
+      navigate("/auth/login")
+    }
+    else {
+      console.log(user)
+    }
+  }, [user, isLoading])
+
+  // Get all of the user's chat messages from the database
+  useEffect(() => {
+    const fetchAllChatMessages = async () => {
+      try {
+        // Put all of the chat messages in the 'userChatMessages' variable if successful
+        const res = await axios.get(`http://localhost:8000/chats/messages/get/${chatId}`, { withCredentials: true })
+        setUserChatMessages(res.data)
+
+      } catch (error) {
+
+        // If an error occured, just print it out for now  
+        console.log("Failed to fetch chat messages: ", error.response?.data?.message || error.message)
+      }
+    }
+    fetchAllChatMessages()
+  }, [])
+
+  // Display 'Loading...' instead of the main content while waiting to check if there is a user currently logged in 
+  if (isLoading) return <div>Loading...</div>
+  if (!user) return null
+
   return (
-    <div>chat</div>
+    <div className='flex bg-gray-100 min-h-screen'>
+
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen}/>
+      
+      <main className={`flex-1 ml-14 transition-all duration-200 ${isOpen ? 'lg:ml-64' : 'lg:ml-14'}`}> 
+        <header className='bg-white flex justify-end p-4 shadow'>
+          <div className='bg-gray-300 w-10 h-10 rounded-full'></div>
+        </header>
+      </main>
+
+    </div>
   )
 }
 
