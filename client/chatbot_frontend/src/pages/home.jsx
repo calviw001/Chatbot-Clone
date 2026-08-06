06493d4c3b4e7971c4c1844730cb0bdf31ab9ef5
calviw001 <cowvynyia@gmail.com>
2026-08-06 13:27:10 -0700

    Added an input text area to the home page and started the 'handleNewChatAndFirstMessage()' function
    This will be where the user will ask there first question and then, after clicking on the add button, be sent to a new chat page that will answer that first question plus many more. The function I created will soon be finished to deal with this.

--- client/chatbot_frontend/src/pages/home.jsx
+++ client/chatbot_frontend/src/pages/home.jsx
@@ -1,5 +1,6 @@
 import {useEffect, useState, useContext} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { GoPlus } from "react-icons/go";
 import axios from 'axios'
 import UserContext from '../context/UserContext'
 import Sidebar from '../components/sidebar'
@@ -8,6 +9,7 @@ import Sidebar from '../components/sidebar'
 const Home = () => {
   // Initialize all variables
   const [isOpen, setIsOpen] = useState(true)
+  const [inputMessage, setInputMessage] = useState("")
   const {user, isLoading} = useContext(UserContext)
 
   const navigate = useNavigate()
@@ -23,6 +25,25 @@ const Home = () => {
     }
   }, [user, isLoading])
 
+  // Create a new chat, redirect the user the page for the new chat, and then save and display the first user message that was entered in beforehand 
+  const handleNewChatAndFirstMessage = async e => {
+      e.preventDefault()
+
+    try {
+      // Throw an error if the user failed to enter in a question
+      if (!inputMessage.trim()) {
+          throw new Error("Please ask a question.")
+      }
+
+      // Clear the input box
+      setInputMessage("")
+    } catch (error) {
+
+      // If an error occured, just print it out for now
+      console.log("Failed to create a new chat: ", error.response?.data?.message || error.message)
+    } 
+  }
+
   // Display 'Loading...' instead of the main content while waiting to check if there is a user currently logged in 
   if (isLoading) return <div>Loading...</div>
   if (!user) return null
@@ -33,12 +54,29 @@ const Home = () => {
       {/*Sidebar*/}
       <Sidebar isOpen={isOpen} setIsOpen={setIsOpen}/>
       
-      <main className={`flex-1 ml-14 transition-all duration-200 ${isOpen ? 'lg:ml-64' : 'lg:ml-14'}`}> 
+      <main className={`flex-1 ml-14 transition-all duration-200 flex flex-col min-h-screen ${isOpen ? 'lg:ml-64' : 'lg:ml-14'}`}> 
         
         {/*Header bar*/}
         <header className='bg-white sticky top-0 z-10 flex justify-end p-4 shadow'>
           <div className='bg-gray-300 w-10 h-10 rounded-full'></div>
         </header>
+
+
+        {/*Add message input box and add button*/}
+        <div className='flex-1 flex items-center justify-center'>
+          <div className='bg-white flex flex-col p-4 rounded-xl max-w-3xl w-full mx-auto'>
+            <textarea
+              name="userMessage"
+              id="userMessage"
+              placeholder="Ask Anything"
+              rows={1}
+              className='flex-1 field-sizing-content resize-none p-2 outline-none focus:outline-none max-h-40 overflow-y-auto'
+              value={inputMessage}
+              onChange={(e) => setInputMessage(e.target.value)}
+            />
+            <button className='flex justify-start cursor-pointer' onClick={handleNewChatAndFirstMessage}><GoPlus size={24}/></button>
+          </div>
+        </div>
 
       </main>
 
